d67e89299215b8efd03a51123ec534620e9a3f94
Steven Harradine <stevenharradine@gmail.com>
2021-06-19 09:35:08 -0400

    format the display price with commas for all the prices not just usd converted ones

--- content-generators/finance/view.sh
+++ content-generators/finance/view.sh
@@ -1,10 +1,10 @@
 echo -n "<break:new><size:16px>$title<break:line>$unitSign"
 
 if [ "$convertUSD2CAD" = "true" ]; then
-	echo "$price * $cad2usd" | bc | xargs printf "%'.2f"
-else
-	echo "$price"
+	price=`echo "$price * $cad2usd" | bc`
 fi
+
+printf "%'.2f" $price
 
 echo -n "<break:new><size:32px> $image <break:new><size:16px>"
 
