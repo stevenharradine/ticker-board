7072cb4ab21c5f6b1c60d574aae1ffd97ebbcadf
Steven Harradine <stevenharradine@gmail.com>
2022-02-28 20:08:30 -0500

    clean up

--- content-generators/sports.sh
+++
@@ -1,6 +0,0 @@
-#curl --location --request POST 'https://oauth2.elenasport.io/oauth2/token' \
-#    --header 'Content-Type: application/x-www-form-urlencoded' \
-#    --header 'Authorization: Basic MmYwdWNtMzRvOGhzNW81OGJxM200cmY2YjU6djVtZGkxbzNmdmY3NzgxMDk0aHM3Y2psMWU2NDE2MXBsbmFxMHJsYWRpcDV2bGZpb2xw' \
-#    --data-urlencode 'grant_type=client_credentials' > cache
-
-cat cache | jq
