a8baf2d6cbc3317bc4501129c421ad97bdd2c051
Steven Harradine <stevenharradine@gmail.com>
2022-03-01 19:53:53 -0500

    refactor

--- content-generators/finance.sh
+++ content-generators/finance.sh
@@ -3,17 +3,14 @@ size="32px"
 tsx=`cat settings.json | grep "\"tsx\":" | cut -d":" -f2 | tr -d ",[:space:]"`
 djia=`cat settings.json | grep "\"djia\":" | cut -d":" -f2 | tr -d ",[:space:]"`
 nasdaq=`cat settings.json | grep "\"nasdaq\":" | cut -d":" -f2 | tr -d ",[:space:]"`
-gold=`cat settings.json | grep "\"gold\":" | cut -d":" -f2 | tr -d ",[:space:]"`
-silver=`cat settings.json | grep "\"silver\":" | cut -d":" -f2 | tr -d ",[:space:]"`
-platinum=`cat settings.json | grep "\"platinum\":" | cut -d":" -f2 | tr -d ",[:space:]"`
-palladium=`cat settings.json | grep "\"palladium\":" | cut -d":" -f2 | tr -d ",[:space:]"`
+au=`cat settings.json | grep "\"au\":" | cut -d":" -f2 | tr -d ",[:space:]"`
+ag=`cat settings.json | grep "\"ag\":" | cut -d":" -f2 | tr -d ",[:space:]"`
+pt=`cat settings.json | grep "\"pt\":" | cut -d":" -f2 | tr -d ",[:space:]"`
+pd=`cat settings.json | grep "\"pd\":" | cut -d":" -f2 | tr -d ",[:space:]"`
 btc=`cat settings.json | grep "\"btc\":" | cut -d":" -f2 | tr -d ",[:space:]"`
 eth=`cat settings.json | grep "\"eth\":" | cut -d":" -f2 | tr -d ",[:space:]"`
 xrp=`cat settings.json | grep "\"xrp\":" | cut -d":" -f2 | tr -d ",[:space:]"`
 xlm=`cat settings.json | grep "\"xlm\":" | cut -d":" -f2 | tr -d ",[:space:]"`
-huv=`cat settings.json | grep "\"huv\":" | cut -d":" -f2 | tr -d ",[:space:]"`
-pea=`cat settings.json | grep "\"pea\":" | cut -d":" -f2 | tr -d ",[:space:]"`
-lcid=`cat settings.json | grep "\"lcid\":" | cut -d":" -f2 | tr -d ",[:space:]"`
 
 source content-generators/finance/scrapers/market.sh currency usdcad
 cad2usd="$price"
@@ -52,49 +49,37 @@ if [ "$oil" == "true" ]; then
 	source content-generators/finance/view.sh
 fi
 
-if [ "$gold" == "true" ]; then
-	source content-generators/finance/scrapers/metal.sh AU
-fi
+metals=( "au" "ag" "pt" "pd" )
+for i in "${!metals[@]}"; do
+	metal=${metals[$i]}
 
-if [ "$silver" == "true" ]; then
-	source content-generators/finance/scrapers/metal.sh AG
-fi
+	if [ ${!metal} == "true" ]; then
+		source content-generators/finance/scrapers/metal.sh "$metal"
+	fi
+done
 
-if [ "$platinum" == "true" ]; then
-	source content-generators/finance/scrapers/metal.sh PT
-fi
+cryptos=( "btc" "eth" "xlm" "xrp" )
+for i in "${!cryptos[@]}"; do
+	crypto=${cryptos[$i]}
 
-if [ "$palladium" == "true" ]; then
-	source content-generators/finance/scrapers/metal.sh PD
-fi
+	if [ ${!crypto} == "true" ]; then
+		source content-generators/finance/scrapers/crypto.sh "$crypto"-usd
+	fi
+done
 
-if [ "$btc" == "true" ]; then
-	source content-generators/finance/scrapers/crypto.sh btc-usd
-fi
+equityType=(   "fund" "stock" "stock" "stock" "stock" "stock" "fund")
+equityTicker=( "huv"  "pea"   "lcid"  "rvin"  "duol"  "abt"   "hack")
+equityMarket=( "ca"   "ca"    "us"    "us"    "us"    "us"    "us")
 
-if [ "$eth" == "true" ]; then
-	source content-generators/finance/scrapers/crypto.sh eth-usd
-fi
+for i in "${!equityType[@]}"; do
+	type=${equityType[$i]}
+	ticker=${equityTicker[$i]}
+	market=${equityMarket[$i]}
 
-if [ "$xlm" == "true" ]; then
-	source content-generators/finance/scrapers/crypto.sh xlm-usd
-fi
+	isTickerEnabled=`cat settings.json | grep -E "\"${ticker}\":" | cut -d":" -f2 | tr -d ",[:space:]"`
 
-if [ "$xrp" == "true" ]; then
-	source content-generators/finance/scrapers/crypto.sh xrp-usd
-fi
-
-if [ "$huv" == "true" ]; then
-	source content-generators/finance/scrapers/market.sh fund "huv" ca
-	source content-generators/finance/view.sh
-fi
-
-if [ "$pea" == "true" ]; then
-	source content-generators/finance/scrapers/market.sh stock "pea" ca
-	source content-generators/finance/view.sh
-fi
-
-if [ "$lcid" == "true" ]; then
-	source content-generators/finance/scrapers/market.sh stock "lcid"
-	source content-generators/finance/view.sh
-fi
+	if [ "$isTickerEnabled" == "true" ]; then
+		source content-generators/finance/scrapers/market.sh "$type" "$ticker" "$market"
+		source content-generators/finance/view.sh
+	fi
+done
